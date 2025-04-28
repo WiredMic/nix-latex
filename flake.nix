10e1aa9110b3c18f9b7d9deb02049a7a2d3beb58
Rasmus Enevoldsen <rasmus@enev.dk>
2025-04-28 22:13:41 +0200

    feat: liberius font

--- flake.nix
+++ flake.nix
@@ -1,46 +1,5 @@
-# {
-#   description = "LaTeX Document Demo";
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-#     flake-utils.url = "github:numtide/flake-utils";
-#   };
-#   outputs = { self, nixpkgs, flake-utils }:
-#     with flake-utils.lib;
-#     eachSystem allSystems (system:
-#       let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#         tex = pkgs.texlive.combine {
-#           inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
-#         };
-#       in rec {
-#         packages = {
-#           document = pkgs.stdenvNoCC.mkDerivation rec {
-#             name = "latex-demo-document";
-#             src = self;
-#             buildInputs = [ pkgs.coreutils tex ];
-#             phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-#             buildPhase = ''
-#               export PATH="${pkgs.lib.makeBinPath buildInputs}";
-#               mkdir -p .cache/texmf-var
-#               SOURCE_DATE_EPOCH=$(date -d "2025-04-30" +%s) \
-#               # make env
-#               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-#                 SOURCE_DATE_EPOCH=$(date -d "2025-04-30" +%s) \
-#                 latexmk -interaction=nonstopmode -pdf -lualatex \
-#                 -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
-#                 -usepretex document.tex
-#             '';
-#             installPhase = ''
-#               rm -f $out
-#               mkdir -p $out
-#               cp document.pdf $out/
-#             '';
-#           };
-#         };
-#         defaultPackage = packages.document;
-#       });
-# }
 {
+  # https://flyx.org/nix-flakes-latex/
   description = "LaTeX Document with Development Environment";
 
   inputs = {
@@ -53,20 +12,26 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+          inherit (pkgs.texlive) scheme-basic latex-bin latexmk fontspec;
         };
       in {
         packages = {
           document = pkgs.stdenvNoCC.mkDerivation rec {
             name = "latex-demo-document";
             src = self;
-            buildInputs = [ pkgs.coreutils tex ];
+            buildInputs = [
+              pkgs.coreutils
+              tex
+              pkgs.libertinus
+
+            ];
             phases = [ "unpackPhase" "buildPhase" "installPhase" ];
             buildPhase = ''
               export PATH="${pkgs.lib.makeBinPath buildInputs}";
               mkdir -p .cache/texmf-var
               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
                 SOURCE_DATE_EPOCH=$(date -d "2025-04-30" +%s) \
+                OSFONTDIR=${pkgs.libertinus}/share/fonts \
                 latexmk -interaction=nonstopmode -pdf -lualatex \
                 -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
                 -usepretex document.tex
