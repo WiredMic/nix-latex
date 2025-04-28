a533d5263a20fbbdc6f7002968057a065356840b
Rasmus Enevoldsen <rasmus@enev.dk>
2025-04-28 20:40:46 +0200

    feat: working nix devolop

--- flake.nix
+++ flake.nix
@@ -1,18 +1,61 @@
+# {
+#   description = "LaTeX Document Demo";
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+#     flake-utils.url = "github:numtide/flake-utils";
+#   };
+#   outputs = { self, nixpkgs, flake-utils }:
+#     with flake-utils.lib;
+#     eachSystem allSystems (system:
+#       let
+#         pkgs = nixpkgs.legacyPackages.${system};
+#         tex = pkgs.texlive.combine {
+#           inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+#         };
+#       in rec {
+#         packages = {
+#           document = pkgs.stdenvNoCC.mkDerivation rec {
+#             name = "latex-demo-document";
+#             src = self;
+#             buildInputs = [ pkgs.coreutils tex ];
+#             phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+#             buildPhase = ''
+#               export PATH="${pkgs.lib.makeBinPath buildInputs}";
+#               mkdir -p .cache/texmf-var
+#               SOURCE_DATE_EPOCH=$(date -d "2025-04-30" +%s) \
+#               # make env
+#               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+#                 SOURCE_DATE_EPOCH=$(date -d "2025-04-30" +%s) \
+#                 latexmk -interaction=nonstopmode -pdf -lualatex \
+#                 -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
+#                 -usepretex document.tex
+#             '';
+#             installPhase = ''
+#               rm -f $out
+#               mkdir -p $out
+#               cp document.pdf $out/
+#             '';
+#           };
+#         };
+#         defaultPackage = packages.document;
+#       });
+# }
 {
-  description = "LaTeX Document Demo";
+  description = "LaTeX Document with Development Environment";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib;
-    eachSystem allSystems (system:
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         tex = pkgs.texlive.combine {
           inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
         };
-      in rec {
+      in {
         packages = {
           document = pkgs.stdenvNoCC.mkDerivation rec {
             name = "latex-demo-document";
@@ -22,18 +65,33 @@
             buildPhase = ''
               export PATH="${pkgs.lib.makeBinPath buildInputs}";
               mkdir -p .cache/texmf-var
-              SOURCE_DATE_EPOCH=$(date -d "2021-11-30" +%s) \
-              # make env
               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                SOURCE_DATE_EPOCH=$(date -d "2025-04-30" +%s) \
                 latexmk -interaction=nonstopmode -pdf -lualatex \
-                document.tex
+                -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
+                -usepretex document.tex
             '';
             installPhase = ''
               mkdir -p $out
               cp document.pdf $out/
             '';
           };
+          default = self.packages.${system}.document;
         };
-        defaultPackage = packages.document;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # LaTeX
+            tex
+
+            # LaTeX LSP and formatter
+            texlab
+            tex-fmt
+
+            # PDF reader
+            okular
+
+          ];
+        };
       });
 }
